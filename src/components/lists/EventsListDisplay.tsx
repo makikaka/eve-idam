a3ae5b2d32e0179d67cce936d98b6b143f12e499
makikaka <mladen.tasevski@gmail.com>
2024-10-08 19:57:41 +0200

    How to mongo

--- src/components/lists/EventsListDisplay.tsx
+++ src/components/lists/EventsListDisplay.tsx
@@ -10,7 +10,7 @@ const EventsListDisplay: React.FC<Props> = ({ entities: entities }) => {
     
     const events = entities as Event[]
     return (
-        entities.length > 0 ?
+        events.length > 0 ?
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center">
                 {events.map((event) => (
                     <EntityCard key={event.id} image={event.image} date={event.date} location={event.location} name={event.name} />
